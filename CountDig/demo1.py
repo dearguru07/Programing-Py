c1ace02cd6b07b77583383ad2b0be89c11ba0279
dearguru07 <pangalaguruyadav101@gmail.com>
2024-10-13 09:52:31 +0530

    npm

--- CountDig/demo1.py
+++ CountDig/demo1.py
@@ -1,15 +1,15 @@
 
 # -----------using Function for Countig the digits-----------
 
-# def Digits(n):
-#     count=0
-#     while n>0:
-#         n=n//10
-#         count+=1
-#     return count
-# n=int(input('enter a num'))
-# res=Digits(n) 
-# print(res)   
+def Digits(n):
+    count=0
+    while n>0:
+        n=n//10
+        count+=1
+    return count
+n=int(input('enter a num'))
+res=Digits(n) 
+print(res)   
 
 # -----------using Function for Countig the digits(in user given range)-----------
 
--- ReverseNu/abc.py
+++ ReverseNu/abc.py
@@ -15,14 +15,16 @@
 #     n=n//10
 # print(rev) 
 
-# def Reverse(n):
-#     rev=0
-#     while n!=0:
-#         rem=n%10
-#         rev=(rev*10)+rem
-#         n=n//10
-#     return rev
-# n=int(input('enter a numb'))
-# res=Reverse(n)
-# print(res)    
+
+
+def Reverse(n):
+    rev=0
+    while n!=0:
+        rem=n%10
+        rev=(rev*10)+rem
+        n=n//10
+    return rev
+n=int(input('enter a numb'))
+res=Reverse(n)
+print(res)    
 
